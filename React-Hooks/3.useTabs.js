7d6fb5b32f32b147782f6d36a1cadd0121c3670c
younhoso <thdbsgh3443@naver.com>
2019-11-02 18:10:38 +0900

    191102 커밋

--- React-Hooks/3.useTabs.js
+++ React-Hooks/3.useTabs.js
@@ -11,6 +11,10 @@ const content = [
     {
         tab: "Section2",
         contentTit: "I'm the content of the Section 2"
+    },
+    {
+        tab: "Section3",
+        contentTit: "I'm the content of the Section 3"
     }
 ];
 
@@ -18,6 +22,11 @@ const useTabs = (initialTab, allTabs) => {
     if(!allTabs || !Array.isArray(allTabs)){ //배열이 아닐때 true된다.
         return;
     }
+    /* useState는 
+    첫번째는 state를 초기화 시켜준다.
+    두번째는 첫번째 currentIndex는 index가 될꺼다.
+    그리고 아이템인 setCurrentIndex는 value를 바꿔준다.
+    */
     const [currentIndex , setCurrentIndex ] = useState(initialTab)
     return {
         currentItem: allTabs[currentIndex],
---
+++ React-Hooks/4.useEffect.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const sayHello = () => console.log("hello");
+  const [number, setNumber] = useState(0);
+  const [aNumber, setAnumber] = useState(0);
+  /**
+   * useEffect는 
+   * ComponentDidMount, ComponentWillUnMount, ComponentDidUpdate 와 동일하다.
+   * 이것 모두 상태변화를 감시할거고, componenet는 unmount될거다.
+   */
+  useEffect(sayHello, [number]);
+  return (
+    <div className="App">
+      <div>Hi</div>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
